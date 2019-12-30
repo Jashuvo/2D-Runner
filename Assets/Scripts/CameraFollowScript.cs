08314fcbda128b2e7e4a5656fb498f565c38a6ab
Jashuvo <jubayedsr@gmail.com>
2019-12-30 15:01:14 +0600

    Initial commit

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform playerObject;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraFollow();
+    }
+
+    void CameraFollow()
+    {
+        transform.position = playerObject.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody playerRigid;
+    public GameObject[] roadCreator;
+
+
+    
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        playerRigid.velocity = new Vector3(0, 0, 25);
+    }
+
+    
+    private void OnTriggerEnter(Collider collsion)
+    {
+        int random = Random.Range(0, 5);
+        if (collsion.gameObject.tag == "RoadCreator")
+        {
+            Instantiate(roadCreator[random], new Vector3(0, 0, 750), Quaternion.identity);
+        }
+    }
+}
